b3a59d778e9dcc4edc3cb798b279d7df404d530d
Cindy Demont <cindyeko.demont@hes-so.ch>
2025-11-04 18:27:03 +0100

    change

--- public/index.php
+++ public/index.php
@@ -16,7 +16,7 @@
         <a href="calendar.php">Calendrier Concerts</a>
         <a href="blog.php">Blog</a>
         <a href="connexion.php">Connexion</a>
-        <a href="test_db_indey.php">TEST</a>
+        <a href="test_db_index.php">TEST</a>
     </div>
 </nav>
 
